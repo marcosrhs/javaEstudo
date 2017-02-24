d71adb2f41bea696d1859893c083b7c8eafed93a
alex <alex@alex-PC>
2017-02-24 12:55:40 -0300

    gantt chart

---
+++ src/servlet/BuscarDatasPlanejamento.java
@@ -0,0 +1,51 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import dao.DaoGanttChart;
+import entidades.Projeto;
+
+@WebServlet("/pages/buscarDatasPlanejamento")
+public class BuscarDatasPlanejamento extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private DaoGanttChart daoGanttChart = new DaoGanttChart();
+
+	public BuscarDatasPlanejamento() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		try {
+
+			List<Projeto> projetos = daoGanttChart.getProjetos();
+
+			if (!projetos.isEmpty()) {
+				String grantJson = new Gson().toJson(projetos);
+	
+				response.setStatus(200);
+				response.getWriter().write(grantJson);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
